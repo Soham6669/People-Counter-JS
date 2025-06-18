d2c649009b4984ed4a8ef0005298214f1ff7f26f
Soham6669 <sohamvedpathak0@gmail.com>
2025-06-19 02:59:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// document.getElementById("count").innerText = 5
+let saveEL = document.getElementById("save-el")
+let Count = document.getElementById("count")
+
+console.log(Count)
+
+let count = 0
+
+function increment(){
+    count += 1
+    Count.innerText = count
+    // console.log(count)
+} 
+
+function decrement(){
+    count -= 1
+    Count.innerText = count
+}
+
+function save(){
+    let a = count + " - "
+
+    saveEL.textContent += a 
+
+    Count.textContent = 0
+    count = 0
+
+}
+
+
+// save()
+
